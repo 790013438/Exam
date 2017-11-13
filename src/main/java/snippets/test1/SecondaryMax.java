47338ffe57bc2f11874499fd709fb42a3ede0780
790013438 <790013438@qq.com>
2017-11-13 15:37:02 +0800

    使用接口减少重复代码哦
    比较是一个接口，里面只有一方法，比较，谁调用谁实现，传接口的内部类，类似KeyListener

--- src/main/java/snippets/test1/SecondaryMax.java
+++ src/main/java/snippets/test1/SecondaryMax.java
@@ -7,47 +7,43 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class SecondaryMax {
+class SecondaryMax {
 
-    public static <T extends Comparable<T>> T secondaryMax(T[] array) {
+    private static <T> T _secondaryMax(T[] array, CompareOperation<T> compareOperation) {
+        @SuppressWarnings("unchecked")
         Set<T> set = new HashSet(Arrays.asList(array));
         List<T> list = new ArrayList<>(set);
         T maxT = list.get(0);
         T seT = list.get(1);
-        if (maxT.compareTo(seT) < 0) {
+        if (compareOperation.compare(maxT, seT) < 0) {
             maxT = returnFirst(seT, seT = maxT);
         }
         for (int i = 3; i < list.size(); ++i) {
-            if (seT.compareTo(list.get(i)) < 0) {
+            if (compareOperation.compare(seT, list.get(i)) < 0) {
                 seT = list.get(i);
             }
-            if (maxT.compareTo(seT) < 0) {
+            if (compareOperation.compare(maxT, seT) < 0) {
                 maxT = returnFirst(seT, seT = maxT);
             }
         }
         return seT;
     }
 
-    public static <T> T returnFirst(T o1, T o2) {
+    static <T extends Comparable<T>> T secondaryMax(T[] array) {
+        return _secondaryMax(array, Comparable::compareTo);
+    }
+
+    @SuppressWarnings("trick")
+    private static <T> T returnFirst(T o1, T o2) {
         return o1;
     }
 
-    public static <T> T secondaryMax(T[] array, Comparator<T> comp) {
-        Set<T> set = new HashSet<>(Arrays.asList(array));
-        List<T> list = new ArrayList<>(set);
-        T maxT = list.get(0);
-        T seT = list.get(1);
-        if (comp.compare(maxT, seT) < 0) {
-            maxT = returnFirst(seT, seT = maxT);
-        }
-        for (int i = 2; i < list.size(); ++i) {
-            if (comp.compare(seT, list.get(i)) < 0) {
-                seT = list.get(i);
-            }
-            if (comp.compare(maxT, seT) < 0) {
-                maxT = returnFirst(seT, seT = maxT);
-            }
-        }
-        return seT;
+    static <T> T secondaryMax(T[] array, Comparator<T> comp) {
+        return _secondaryMax(array, comp::compare);
+    }
+
+    @FunctionalInterface
+    private interface CompareOperation<T> {
+        int compare(T o1, T o2);
     }
 }
