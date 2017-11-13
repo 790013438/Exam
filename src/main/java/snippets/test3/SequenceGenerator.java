a00875929b9e270ed084e8b917f00d3dfdea749b
790013438 <790013438@qq.com>
2017-11-13 16:34:10 +0800

    换成AtomicLong

--- src/main/java/snippets/test3/SequenceGenerator.java
+++ src/main/java/snippets/test3/SequenceGenerator.java
@@ -1,25 +1,17 @@
 package snippets.test3;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
-import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
-public final class SequenceGenerator {
+final class SequenceGenerator {
 
-    private AtomicInteger key;
+    private AtomicLong key;
 
-    public synchronized String next(int start, int delta) {
+    String next(int start, int delta) {
         if (key == null) {
-            key = new AtomicInteger(start);
+            key = new AtomicLong(start);
         }
 
-        return Integer.toString(key.getAndAdd(delta));
+        return Long.toString(key.getAndAdd(delta));
     }
 
 }
