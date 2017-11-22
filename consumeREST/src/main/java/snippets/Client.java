1a3d4d462b62a5c33af858374f7fcaf1094fcad3
790013438 <790013438@qq.com>
2017-11-22 20:15:25 +0800

    中文参数就是中文参数，Jackson不用转码

--- consumeREST/src/main/java/snippets/Client.java
+++ consumeREST/src/main/java/snippets/Client.java
@@ -7,7 +7,9 @@ public class Client {
     public static void main(String[] args) {
 
         RestTemplate restTemplate = new RestTemplate();
-        ModelData modelData = restTemplate.getForObject("http://api.avatardata.cn/MingRenMingYan/LookUp?key=56ca7b9e4aee431a82bae759161d5905&keyword=%E5%A4%A9%E6%89%8D&page=1&rows=15", ModelData.class);
-        System.out.println(modelData.getResult());
+        ModelData modelData = restTemplate.getForObject("http://api.avatardata.cn/MingRenMingYan/LookUp?key=56ca7b9e4aee431a82bae759161d5905&keyword=天才&page=1&rows=15", ModelData.class);
+
+        for (Result result : modelData.getResult())
+            System.out.println(result.getFamousSaying());
     }
 }
